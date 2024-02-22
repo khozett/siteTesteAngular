a86e51d99d078da4c2b3218d50e8bc1b419e0b92
khozett <sergiodaconceicaosouza@gmail.com>
2024-02-22 01:02:02 -0300

    attCabecario-Form-Json

---
+++ src/app/seller-auth/seller-auth.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-seller-auth',
+  templateUrl: './seller-auth.component.html',
+  styleUrls: ['./seller-auth.component.css']
+})
+export class SellerAuthComponent implements OnInit {
+
+  constructor(){}
+
+
+  ngOnInit(): void {}
+
+    signUp(data:Object):void{
+
+      console.warn(data)
+
+    }
+
+
+
+
+}
